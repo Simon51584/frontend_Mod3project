54874978a2f32c5a01bbbb952bb698b55933b187
Sam M <sammontalvojr@gmail.com>
2020-08-17 15:23:01 -0500

    starting JS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-function Main(){
+const grid = document.querySelector("#div-grid");
+let squares;
+const ScoreDisplay = document.querySelector("#score");
+const StartBtn = document.querySelector("#start-button");
+const width = 10;
 
-}
+const main = () => {
+  renderGrid();
+};
 
-Main()
+const renderGrid = () => {
+  let boxes = "";
+  for (let i = 0; i < 200; i++) {
+    boxes += "<div></div>";
+  }
+  grid.innerHTML = boxes;
+  squares = Array.from(document.querySelectorAll(".grid div"));
+  console.log(squares);
+};
+
+main();

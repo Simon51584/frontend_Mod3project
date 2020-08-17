f6f82127b3cccfc14ba7056944d078ca5d996399
Sam M <sammontalvojr@gmail.com>
2020-08-17 18:38:29 -0500

    rotating L mapped to the space bar

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ const grid = document.querySelector("#div-grid");
 let squares;
 const ScoreDisplay = document.querySelector("#score");
 const StartBtn = document.querySelector("#start-button");
-const width = 10;
+const columnsPerRow = 10;
+
+const lTetramino = [
+  [1, columnsPerRow + 1, columnsPerRow * 2 + 1, 2],
+  [1, 2, 3, columnsPerRow + 3],
+  [columnsPerRow * 2 + 1, 2, columnsPerRow + 2, columnsPerRow * 2 + 2],
+  [1, columnsPerRow + 1, columnsPerRow + 2, columnsPerRow + 3],
+];
+const allTetraminos = [lTetramino];
+
+let currentPosition = 5;
+let currentRotation = 0;
+const currentTetramino = () => allTetraminos[0][currentRotation];
 
 const main = () => {
   renderGrid();
+  drawTetramino();
 };
 
 const renderGrid = () => {
@@ -15,7 +28,35 @@ const renderGrid = () => {
   }
   grid.innerHTML = boxes;
   squares = Array.from(document.querySelectorAll(".grid div"));
-  console.log(squares);
+  gameListeners();
+};
+
+const drawTetramino = () => {
+  currentTetramino().forEach((index) => {
+    squares[currentPosition + index].classList.add("tetramino");
+  });
+};
+
+const removeTetramino = () => {
+  currentTetramino().forEach((index) => {
+    squares[currentPosition + index].classList.remove("tetramino");
+  });
+};
+
+const gameListeners = () => {
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Space") {
+      removeTetramino();
+      console.log(currentRotation);
+      if (currentRotation === 3) {
+        currentRotation = 0;
+        drawTetramino();
+      } else {
+        currentRotation += 1;
+        drawTetramino();
+      }
+    }
+  });
 };
 
 main();

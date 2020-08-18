d7cace28c1a364c6f39e9b208716fba22c90cd6f
Sam M <sammontalvojr@gmail.com>
2020-08-18 18:39:08 -0500

    upper limit function: prevents shape from passing the right limit

--- src/index.js
+++ src/index.js
@@ -2,77 +2,17 @@ const grid = document.querySelector("#div-grid");
 let squares;
 const ScoreDisplay = document.querySelector("#score");
 const StartBtn = document.querySelector("#start-button");
-const columnsPerRow = 10;
 
-const lTetramino = [
-  [1, columnsPerRow + 1, columnsPerRow * 2 + 1, columnsPerRow * 2 + 2],
-  [0, 1, 2, columnsPerRow],
-  [1, 2, columnsPerRow + 2, columnsPerRow * 2 + 2],
-  [2, columnsPerRow, columnsPerRow + 1, columnsPerRow + 2],
-];
-
-const jTetramino = [
-  [1, columnsPerRow + 1, columnsPerRow * 2 + 1, 2],
-  [1, 2, 3, columnsPerRow + 3],
-  [columnsPerRow * 2 + 1, 2, columnsPerRow + 2, columnsPerRow * 2 + 2],
-  [1, columnsPerRow + 1, columnsPerRow + 2, columnsPerRow + 3],
-];
-
-const squareTetramino = [
-  [1, 2, columnsPerRow + 1, columnsPerRow + 2],
-  [1, 2, columnsPerRow + 1, columnsPerRow + 2],
-  [1, 2, columnsPerRow + 1, columnsPerRow + 2],
-  [1, 2, columnsPerRow + 1, columnsPerRow + 2],
-];
-
-const iTetramino = [
-  [0, 1, 2, 3],
-  [0, columnsPerRow, columnsPerRow * 2, columnsPerRow * 3],
-  [0, 1, 2, 3],
-  [0, columnsPerRow, columnsPerRow * 2, columnsPerRow * 3],
-];
-
-const zTetramino = [
-  [0, 1, columnsPerRow + 1, columnsPerRow + 2],
-  [columnsPerRow, 1, columnsPerRow + 1, columnsPerRow * 2],
-  [0, 1, columnsPerRow + 1, columnsPerRow + 2],
-  [columnsPerRow, 1, columnsPerRow + 1, columnsPerRow * 2],
-];
-
-const sTetramino = [
-  [1, 2, columnsPerRow, columnsPerRow + 1],
-  [0, columnsPerRow, columnsPerRow + 1, columnsPerRow * 2 + 1],
-  [1, 2, columnsPerRow, columnsPerRow + 1],
-  [0, columnsPerRow, columnsPerRow + 1, columnsPerRow * 2 + 1],
-];
-
-const tTetramino = [
-  [0, columnsPerRow, columnsPerRow + 1, columnsPerRow * 2],
-  [0, 1, 2, columnsPerRow + 1],
-  [columnsPerRow, 1, columnsPerRow + 1, columnsPerRow * 2 + 1],
-  [columnsPerRow, 1, columnsPerRow + 1, columnsPerRow + 2],
-];
-
-const allTetraminos = [
-  lTetramino,
-  jTetramino,
-  squareTetramino,
-  iTetramino,
-  zTetramino,
-  sTetramino,
-  tTetramino,
-];
-
-let currentPosition = 5;
+let currentPosition = 165;
 let currentRotation = 0;
-const currentTetramino = () => allTetraminos[0][currentRotation];
+const currentTetramino = () => allTetraminos[6][currentRotation];
 
 const main = () => {
-  renderGrid();
+  renderGameBoard();
   drawTetramino();
 };
 
-const renderGrid = () => {
+const renderGameBoard = () => {
   let boxes = "";
   for (let i = 0; i < 200; i++) {
     boxes += "<div></div>";
@@ -88,47 +28,62 @@ const drawTetramino = () => {
   });
 };
 
-const removeTetramino = () => {
+const eraseTetramino = () => {
   currentTetramino().forEach((index) => {
     squares[currentPosition + index].classList.remove("tetramino");
   });
 };
 
+const hitUpperBound = () => {
+  const tetPosition = currentTetramino().map((index) =>
+    parseInt((index + currentPosition).toString().slice(-1))
+  );
+  const upperBound = Math.max(...tetPosition);
+  return upperBound === 9;
+};
+
 const gameListeners = () => {
   document.addEventListener("keydown", (event) => {
-    console.log(event);
     switch (event.code) {
       case "ShiftRight":
-        removeTetramino();
         if (currentRotation === 3) {
+          eraseTetramino();
           currentRotation = 0;
           drawTetramino();
         } else {
+          eraseTetramino();
           currentRotation += 1;
           drawTetramino();
         }
         break;
       case "ShiftLeft":
-        removeTetramino();
         if (currentRotation === 0) {
+          eraseTetramino();
           currentRotation = 3;
           drawTetramino();
         } else {
+          eraseTetramino();
           currentRotation -= 1;
           drawTetramino();
         }
         break;
       case "ArrowLeft":
-        removeTetramino();
+        eraseTetramino();
         currentPosition -= 1;
         drawTetramino();
         break;
       case "ArrowRight":
-        removeTetramino();
-        currentPosition += 1;
+        eraseTetramino();
+        hitUpperBound()
+          ? (currentPosition = currentPosition)
+          : (currentPosition += 1);
         drawTetramino();
         break;
-
+      case "ArrowUp":
+        eraseTetramino();
+        currentPosition -= 10;
+        drawTetramino();
+        break;
       default:
     }
   });
